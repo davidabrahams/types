8b97a6d42d5b158ce0c1d3afcb2f337d8761befc
David Abrahams <da.j.abrahams@gmail.com>
2019-11-11 01:48:37 -0800

    implement foldL and reverse on our hlist

--- src/main/scala/HList.scala
+++ src/main/scala/HList.scala
@@ -10,6 +10,11 @@ sealed trait HList {
       init: Init,
       fold: F
   ): Foldr[Init, F, Acc]
+  type Foldl[Init <: Acc, F <: Fold[Acc, Any], Acc] <: Acc
+  def foldl[Init <: Acc, F <: Fold[Acc, Any], Acc](
+      init: Init,
+      fold: F
+  ): Foldl[Init, F, Acc]
 }
 
 object HList {
@@ -22,6 +27,14 @@ object HList {
   // TODO: reference to generated companion object for HCons, so we can use
   // its unapply() method I think?
   val :: : HCons.type = HCons
+  // we could just let type inference figure out the type of reverse()
+  // but it's more fun to define a type level Reverse function, and ensure
+  // our implementation is correct
+  type Reverse[A <: HList] = A#Foldl[HNil, ConsFold.type, HList]
+  implicit class HListOps[H <: HList](val h: H) extends AnyVal {
+    def reverse: Reverse[H] =
+      h.foldl[HNil, ConsFold.type, HList](HNil, ConsFold)
+  }
 }
 
 import HList._
@@ -35,7 +48,14 @@ final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
       init: Init,
       fold: F
   ): Foldr[Init, F, Acc] =
-    fold.apply(tail.foldr[Init, F, Acc](init, fold), head)
+    fold(tail.foldr[Init, F, Acc](init, fold), head)
+  // to implement a fold left, we combine H into the "Init" for the next recursion
+  type Foldl[Init <: Acc, F <: Fold[Acc, Any], Acc] =
+    T#Foldl[F#Apply[Init, H], F, Acc]
+  def foldl[Init <: Acc, F <: Fold[Acc, Any], Acc](
+      init: Init,
+      fold: F
+  ) = tail.foldl(fold(init, head), fold)
 }
 
 sealed trait HNil extends HList {
@@ -45,19 +65,42 @@ sealed trait HNil extends HList {
       init: Init,
       fold: F
   ): Init = init
+  type Foldl[Init <: Acc, F <: Fold[Acc, Any], Acc] = Init
+  def foldl[Init <: Acc, F <: Fold[Acc, Any], Acc](
+      init: Init,
+      fold: F
+  ): Init = init
 }
 
-final object HNil extends HNil
+object HNil extends HNil {}
+
+// A Fold which simply Cons the new element on the front of the accumulator
+object ConsFold extends Fold[HList, Any] {
+  type Apply[H <: HList, N <: Any] = N :: H
+
+  // used later for value-level implementations
+  def apply[B <: HList, A](b: B, a: A) = HCons(a, b)
+}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val x: String :: Boolean :: HNil = "str" :: true :: HNil
+    val y: Boolean :: String :: HNil = x.reverse
     x match {
       // these two cases are equivalent
       // case HCons("str", HCons(true, HNil)) => println("yup2")
       case "str" :: true :: _ => println("yup")
       case _                  => println("nope")
     }
+    y match {
+      // these two cases are equivalent
+      // case HCons("str", HCons(true, HNil)) => println("yup2")
+      case true :: "str" :: _ => println("yup")
+      case _                  => println("nope")
+    }
+    type Foo = String :: Boolean :: HNil
+    type Bar = Boolean :: String :: HNil
+    // test our reverse implementation is correct
+    implicitly[Reverse[Foo] =:= Bar]
   }
-
 }
